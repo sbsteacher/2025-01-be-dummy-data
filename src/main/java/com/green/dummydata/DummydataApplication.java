d4ff0bfa29445ddb3d478f1d1018e7c6fbf5f2ac
sbsteacher <id@gmail.com>
2025-07-16 09:57:08 +0900

    first

---
+++ src/main/java/com/green/dummydata/DummydataApplication.java
@@ -0,0 +1,13 @@
+package com.green.dummydata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummydataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DummydataApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/green/dummydata/user/UserMapper.java
@@ -0,0 +1,7 @@
+package com.green.dummydata.user;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+}
